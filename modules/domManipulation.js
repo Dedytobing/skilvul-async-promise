08dff3d86c43e702a2c6e9f8ff1bfa0972dfaebc
Dedytobing <101456245+Dedytobing@users.noreply.github.com>
2023-04-12 16:03:50 +0700

    menambahkan button untuk mengurutkan A-Z / Z-A

--- modules/domManipulation.js
+++ modules/domManipulation.js
@@ -1,21 +1,25 @@
 export function domManipulation() {
-    const log = document.querySelector('.event-log');
-    
-    document.querySelector('#xhr-1').addEventListener('click', () => {
-    log.textContent = '';
+  const log = document.querySelector(".event-log");
+
+  document.querySelector("#xhr-1").addEventListener("click", () => {
+    log.textContent = "";
 
     const xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('loadend', () => {
-        log.textContent = `${log.textContent}Finished with status: ${xhr.status}`;
+    xhr.addEventListener("loadend", () => {
+      log.textContent = `${log.textContent}Finished with status: ${xhr.status}`;
     });
 
-    xhr.open('GET', 'https://raw.githubusercontent.com/mdn/content/main/files/en-us/_wikihistory.json');
+    xhr.open(
+      "GET",
+      "https://raw.githubusercontent.com/mdn/content/main/files/en-us/_wikihistory.json"
+    );
     xhr.send();
-    log.textContent = `${log.textContent}Started XHR request\n`;});
+    log.textContent = `${log.textContent}Started XHR request\n`;
+  });
 
-    document.querySelector('#reload-1').addEventListener('click', () => {
-        log.textContent = '';
-        document.location.reload();
-    });
+  document.querySelector("#reload-1").addEventListener("click", () => {
+    log.textContent = "";
+    document.location.reload();
+  });
 }
--- modules/request/product.js
+++ modules/request/product.js
@@ -1,31 +1,105 @@
 export function getProduct() {
-    const result = document.querySelector('#xhr-2-result');
-    
-    document.querySelector('#xhr-2').addEventListener('click', () => {
-        result.textContent = '';
+  const result = document.querySelector("#xhr-2-result");
+
+  document.querySelector("#xhr-2").addEventListener("click", () => {
+    result.textContent = "";
 
     const xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('loadend', () => {
-        let responseText = xhr.responseText
-        let responseJSON = JSON.parse(responseText)
-        let addedHTML = ""
-        for (let i=0; i<responseJSON.length; i++) {
-            addedHTML =  addedHTML + `<li>${responseJSON[i].name}</li>`
-            // console.log(responseJSON[i].name)
-        }
-        console.log(addedHTML)
-        result.innerHTML = `<ul>${addedHTML}</ul>`
+    xhr.addEventListener("loadend", () => {
+      let responseText = xhr.responseText;
+      let responseJSON = JSON.parse(responseText);
+      let addedHTML = "";
 
-        // result.textContent = `${result.textContent}Finished with status: ${xhr.responseText}`;
+      for (let i = 0; i < responseJSON.length; i++) {
+        addedHTML = addedHTML + `<li>${responseJSON[i].name}</li>`;
+      }
+
+      result.innerHTML = `<ul>${addedHTML}</ul>`;
     });
 
-    xhr.open('GET', 'https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
+    xhr.open(
+      "GET",
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    );
     xhr.send();
-    result.textContent = `${result.textContent}Started XHR request\n`;});
+    result.textContent = `${result.textContent}Started XHR request\n`;
+  });
 
-    document.querySelector('#reload-2').addEventListener('click', () => {
-        result.textContent = '';
-        document.location.reload();
+  document.querySelector("#reload-2").addEventListener("click", () => {
+    result.textContent = "";
+    document.location.reload();
+  });
+}
+
+export function ascending() {
+  const result = document.querySelector("#xhr-2-result");
+
+  document.querySelector("#asc").addEventListener("click", () => {
+    result.textContent = "";
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("loadend", () => {
+      let responseText = xhr.responseText;
+      let responseJSON = JSON.parse(responseText);
+      let addedHTML = "";
+      let resp = [];
+
+      for (let i = 0; i < responseJSON.length; i++) {
+        resp.push(responseJSON[i].name);
+      }
+
+      resp.sort();
+
+      for (let i = 0; i < resp.length; i++) {
+        addedHTML = addedHTML + `<li>${resp[i]}</li>`;
+      }
+
+      result.innerHTML = `<ul>${addedHTML}</ul>`;
     });
+
+    xhr.open(
+      "GET",
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    );
+    xhr.send();
+    result.textContent = `${result.textContent}Started XHR request\n`;
+  });
+}
+
+export function descending() {
+  const result = document.querySelector("#xhr-2-result");
+
+  document.querySelector("#desc").addEventListener("click", () => {
+    result.textContent = "";
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("loadend", () => {
+      let responseText = xhr.responseText;
+      let responseJSON = JSON.parse(responseText);
+      let addedHTML = "";
+      let resp = [];
+
+      for (let i = 0; i < responseJSON.length; i++) {
+        resp.push(responseJSON[i].name);
+      }
+
+      resp.sort().reverse();
+
+      for (let i = 0; i < resp.length; i++) {
+        addedHTML = addedHTML + `<li>${resp[i]}</li>`;
+      }
+
+      result.innerHTML = `<ul>${addedHTML}</ul>`;
+    });
+
+    xhr.open(
+      "GET",
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    );
+    xhr.send();
+    result.textContent = `${result.textContent}Started XHR request\n`;
+  });
 }
